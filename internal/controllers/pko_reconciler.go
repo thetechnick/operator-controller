7a2725b19a7d5b7979917bce2c5e4315ce63de0a
Nico Schieder <nschieder@redhat.com>
2024-07-23 09:33:39 +0200

    Parametrize namespaces

--- internal/controllers/pko_reconciler.go
+++ internal/controllers/pko_reconciler.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	ocv1alpha1 "github.com/operator-framework/operator-controller/api/v1alpha1"
@@ -24,6 +25,12 @@ type pkoReconciler struct {
 func (r *pkoReconciler) Reconcile(ctx context.Context, cext *ocv1alpha1.ClusterExtension, bundle *declcfg.Bundle) (
 	res ctrl.Result, err error,
 ) {
+	configJson, err := json.Marshal(map[string]interface{}{
+		"namespace": cext.Spec.InstallNamespace,
+	})
+	if err != nil {
+		return res, err
+	}
 	desiredPkg := &pkocorev1alpha1.ClusterPackage{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "package-operator.run/v1alpha1",
@@ -37,6 +44,9 @@ func (r *pkoReconciler) Reconcile(ctx context.Context, cext *ocv1alpha1.ClusterE
 		},
 		Spec: pkocorev1alpha1.PackageSpec{
 			Image: bundle.Image,
+			Config: &runtime.RawExtension{
+				Raw: configJson,
+			},
 		},
 	}
 	if err := controllerutil.SetControllerReference(cext, desiredPkg, r.scheme); err != nil {
